cba551892e0b8db35230c0a2baa6cea61f7572bd
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-11-30 20:16:04 +0200

    create workspace and add turtlebot3_simulation package

---
+++ build/turtlebot3_simulations/turtlebot3_simulations/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/projects/udacity/SLAM & Mapping/gmapping_WS/src/turtlebot3_simulations/turtlebot3_simulations
+# Build directory: /home/ahmed/projects/udacity/SLAM & Mapping/gmapping_WS/build/turtlebot3_simulations/turtlebot3_simulations
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
